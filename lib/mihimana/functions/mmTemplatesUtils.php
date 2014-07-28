3757c892dda7f243bfa83056359b38a37f94f8af
bruspal <bruspal@bruspal.net>
2014-07-28 08:47:34 +0200

    partialsExists() function added

--- lib/mihimana/functions/mmTemplatesUtils.php
+++ lib/mihimana/functions/mmTemplatesUtils.php
@@ -3,7 +3,7 @@
  * return parsed partial string
  * @param string $templateName template name without extension. partials are located in APPLICATION_DIR/templates/partials with the .php extension by default
  * @param array $variable array of variables ('varName' => 'value, ...). Default: array()
- * @param string $extension file's extension. default: .php 
+ * @param string $extension file's extension. default: .php
  * @return string
  */
 function usePartial($templateName, $variables = array(), $extension = '.php') {
@@ -14,9 +14,13 @@ function usePartial($templateName, $variables = array(), $extension = '.php') {
  * echoing parsed partial
  * @param string $templateName template name without extension. partials are located in APPLICATION_DIR/templates/partials with the .php extension by default
  * @param array $variable array of variables ('varName' => 'value, ...). Default: array()
- * @param string $extension file's extension. default: .php 
+ * @param string $extension file's extension. default: .php
  * @return string
  */
 function renderPartial($templateName, $variables = array(), $extension = '.php') {
     echo usePartial($templateName, $variables, $extension);
+}
+
+function partialExists($partialName) {
+    return file_exists(PARTIALS_DIR.DIRECTORY_SEPARATOR.$partialName.'.php');
 }
