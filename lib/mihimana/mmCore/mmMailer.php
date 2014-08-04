432b20ccd35941cc9a99fcda24dfc7b3b48b3c54
bruspal <bruspal@bruspal.net>
2014-08-04 18:12:19 +0200

    development of the mailer (work in progress)

--- lib/mihimana/mmCore/mmMailer.php
+++ lib/mihimana/mmCore/mmMailer.php
@@ -4,18 +4,49 @@
  *
  * @author bruno
  */
-class mmMailer {
+class mmMailer extends mmObject {
 
     private
-            $mail;
+            $mail,
+            $method = 'smtp',
+            $server = '127.0.0.1',
+            $port = 25;
 
     public function __construct() {
         $this->mail = new PHPMailer();
         //TODO :  add a way to choose sender, for now sendMail
-        $this->mail->isSendmail();
+
+        $this->mail->isSMTP();
+        $this->mail->SMTPDebug = 2;
+        $this->mail->Debugoutput = 'html';
+        $this->mail->Host = $this->server;
+        $this->mail->Port = $this->port;
+        $this->mail->SMTPAuth = false;
     }
 
-    public function sendSimpleMail($from, $to, $subject, $body, $fromName = false) {
+    public function sendSimpleMail($to, $subject, $body) {
+        if (!defined(MAIL_FROM)) throw new mmExceptionConfig('MAIL_FROM is missing in config.php file');
+        if (!defined(MAIL_SERVER)) throw new mmExceptionConfig('MAIL_SERVER is missing in config.php file');
+        if (!defined(MAIL_PORT)) throw new mmExceptionConfig('MAIL_PORT is missing in config.php file');
+
+        if (defined(MAIL_FROM_NAME)) {
+            $fromName = MAIL_FROM_NAME;
+        } else {
+            $fromName = false;
+        }
+        $this->sendTextMail(MAIL_FROM, $to, $subject, $body, $fromName);
+    }
+
+    /**
+     * send text mail
+     * @param type $from adresse
+     * @param type $to
+     * @param type $subject
+     * @param type $body
+     * @param type $fromName (optionel) Nom de
+     * @return boolean
+     */
+    public function sendTextMail($from, $to, $subject, $body, $fromName = false) {
         //mail setup
         if ($fromName === false) {
             $this->mail->setFrom($from);
@@ -28,6 +59,7 @@ class mmMailer {
 
         //send
         if ( ! $this->mail->send()) {
+            echo "<h1>".$this->mail->ErrorInfo."</h1>";
             return false;
         }
         // mail successfuly sent
